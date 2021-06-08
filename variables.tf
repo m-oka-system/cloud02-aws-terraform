fd1d569019e051c4cc4aa72c68daae048548c12b
m-oka-system <m-oka-system@outlook.com>
2021-06-08 14:52:51 +0900

    [clean]不要な変数を削除

--- variables.tf
+++ variables.tf
@@ -7,13 +7,6 @@ variable "aws_region" {
   default = "ap-northeast-1"
 }
 
-variable "tags" {
-  default = {
-    project = "cloud02"
-    env     = "dev"
-  }
-}
-
 # VPC
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
